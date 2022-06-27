67304dcf3893e4d4ef70f1f8505cc04d16f94561
efcruz <eduarda.web.dev@gmail.com>
2022-06-27 10:43:31 +0200

    pie chart

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './nprogress.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import EventGenre from './EventGenre';
 import { OfflineAlert } from './Alert';
 import { getEvents, extractLocations } from './api';
 import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
@@ -36,7 +37,7 @@ class App extends Component {
     }
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.mounted = false;
   }
 
@@ -48,7 +49,7 @@ class App extends Component {
       },
       this.updateEvents(currentLocation, numberOfEvents)
     );
-    
+
   };
 
 
@@ -57,24 +58,24 @@ class App extends Component {
       const locationEvents = (location === 'all') ?
         events :
         events.filter((event) => event.location === location);
-        if (this.mounted) {
-          this.setState({
-            events: locationEvents.slice(0, this.state.numberOfEvents),
-            currentLocation: location,
-          });
-        }
+      if (this.mounted) {
+        this.setState({
+          events: locationEvents.slice(0, this.state.numberOfEvents),
+          currentLocation: location,
         });
       }
+    });
+  }
 
-      getData = () => {
-        const {locations, events} = this.state;
-        const data = locations.map((location)=>{
-          const number = events.filter((event) => event.location === location).length
-          const city = location.split(', ').shift()
-          return {city, number};
-        })
-        return data;
-      };
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return { city, number };
+    })
+    return data;
+  };
 
   render() {
     const { events, locations, numberOfEvents, offlineAlert } = this.state;
@@ -82,23 +83,26 @@ class App extends Component {
     return (
       <div className="App">
         <div className='offline-alert-container'>
-        <OfflineAlert text={offlineAlert} />
+          <OfflineAlert text={offlineAlert} />
         </div>
         <h1 className='page-title'>Meet App</h1>
         <CitySearch locations={locations} updateEvents={this.updateEvents} />
-        <NumberOfEvents 
-            numberOfEvents={numberOfEvents} 
-            updateNumberOfEvents={this.updateNumberOfEvents}/>
-        <ResponsiveContainer height={400} >
-          <ScatterChart
-            margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis type="category" dataKey="city" name="city" />
-            <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
-            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-            <Scatter data={this.getData()} fill="#8884d8" />
-          </ScatterChart>
-        </ResponsiveContainer>
+        <NumberOfEvents
+          numberOfEvents={numberOfEvents}
+          updateNumberOfEvents={this.updateNumberOfEvents} />
+        <div className='data-vis-wrapper'>
+          <EventGenre events={events} />
+          <ResponsiveContainer height={400} >
+            <ScatterChart
+              margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>
         <EventList events={events} />
       </div>
     );
